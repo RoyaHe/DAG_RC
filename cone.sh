49a297e77c1cf1e0a20d51d70ec980e957508a57
RoyaHe <51139394+RoyaHe@users.noreply.github.com>
2024-10-14 00:32:13 +0200

    Update cone.sh

--- cone.sh
+++ cone.sh
@@ -1,47 +1,62 @@
 ## pretrain ConE * NELL 
-'''
-CUDA_VISIBLE_DEVICES=0 python3 main.py --cuda --do_train --do_test \
---tree_data_path /workspace/KGReasoning_Original/data/NELL-betae -n 128 -b 512 -d 800 -g 20 --geo cone \
---dag_data_path /workspace/DAG-QA/data/NELL/Hard \
+CUDA_VISIBLE_DEVICES=0 python3 main.py --cuda --do_train \
+--tree_data_path ./data/NELL-betae -n 128 -b 512 -d 800 -g 20 --geo cone \
+--dag_data_path ./DAG-QA/data/NELL/Hard \
 -lr 0.0001 --max_steps 300001 --cpu_num 0 --valid_steps 60000 --test_batch_size 4 \
 --seed 0 --drop 0.2 --tasks 1p.2p.3p.2i.3i.ip.pi.2in.3in.inp.pin.pni.2u.up.2s.3s.sp.is.us.ins \
-'''
 
 ## test ConE * NELL * Easy
 CUDA_VISIBLE_DEVICES=1 python3 main.py --cuda --do_test \
-    --tree_data_path /workspace/KGReasoning_Original/data/NELL-betae -n 128 -b 512 -d 800 -g 20 --geo cone \
-    --dag_data_path /workspace/DAG-QA/data/NELL/Easy \
+    --tree_data_path ./data/NELL-betae -n 128 -b 512 -d 800 -g 20 --geo cone \
+    --dag_data_path ./DAG-QA/data/NELL/Easy \
     -lr 0.0001 --max_steps 300001 --cpu_num 0 --valid_steps 60000 --test_batch_size 4 \
-    --seed 0 --drop 0.2 --tasks 2s.3s.sp.is.us.ins --checkpoint_path /workspace/DAG-QA/joint_rel/logs/NELL/1p.2p.3p.2i.3i.ip.pi.2in.3in.inp.pin.pni.2u.up.2s.3s.sp.is.us.ins/cone/g-20.0-drop-0.2/2024.09.06-12:06:36 \
+    --seed 0 --drop 0.2 --tasks 2s.3s.sp.is.us.ins --checkpoint_path \
 
+## test ConE * NELL * Hard
+CUDA_VISIBLE_DEVICES=1 python3 main.py --cuda --do_test \
+    --tree_data_path ./data/NELL-betae -n 128 -b 512 -d 800 -g 20 --geo cone \
+    --dag_data_path ./DAG-QA/data/NELL/Hard \
+    -lr 0.0001 --max_steps 300001 --cpu_num 0 --valid_steps 60000 --test_batch_size 4 \
+    --seed 0 --drop 0.2 --tasks 2s.3s.sp.is.us.ins --checkpoint_path \
 
 ## Pretrain ConE * FB15k-237
-'''
-CUDA_VISIBLE_DEVICES=3 python3 main.py --cuda --do_train --do_test \
---tree_data_path /workspace/KGReasoning_Original/data/FB15k-237-betae -n 128 -b 512 -d 800 -g 30 --geo cone \
---dag_data_path /workspace/DAG-QA/data/FB15k-237/Hard \
+CUDA_VISIBLE_DEVICES=3 python3 main.py --cuda --do_train \
+--tree_data_path ./data/FB15k-237-betae -n 128 -b 512 -d 800 -g 30 --geo cone \
+--dag_data_path ./DAG-QA/data/FB15k-237/Hard \
 -lr 0.00005 --max_steps 300001 --cpu_num 0 --valid_steps 30000 --test_batch_size 4 \
 --seed 0 --drop 0.1 --tasks 1p.2p.3p.2i.3i.ip.pi.2in.3in.inp.pin.pni.2u.up.2s.3s.sp.is.us.ins \
-'''
+
 ## test ConE * FB15k-237 * Easy
 CUDA_VISIBLE_DEVICES=2 python3 main.py --cuda --do_test \
-    --tree_data_path /workspace/KGReasoning_Original/data/FB15k-237-betae -n 128 -b 512 -d 800 -g 30 --geo cone \
-    --dag_data_path /workspace/DAG-QA/data/FB15k-237/Easy \
+    --tree_data_path ./data/FB15k-237-betae -n 128 -b 512 -d 800 -g 30 --geo cone \
+    --dag_data_path ./DAG-QA/data/FB15k-237/Easy \
     -lr 0.00005 --max_steps 300001 --cpu_num 0 --valid_steps 30000 --test_batch_size 4 \
-    --seed 0 --drop 0.1 --tasks 2s.3s.sp.is.us.ins --checkpoint_path /workspace/DAG-QA/joint_rel/logs/FB15k-237/1p.2p.3p.2i.3i.ip.pi.2in.3in.inp.pin.pni.2u.up.2s.3s.sp.is.us.ins/cone/g-30.0-drop-0.1/2024.09.06-12:11:01 \
-
+    --seed 0 --drop 0.1 --tasks 2s.3s.sp.is.us.ins --checkpoint_path \
+    
+## test ConE * FB15k-237 * Easy
+CUDA_VISIBLE_DEVICES=2 python3 main.py --cuda --do_test \
+    --tree_data_path ./data/FB15k-237-betae -n 128 -b 512 -d 800 -g 30 --geo cone \
+    --dag_data_path ./DAG-QA/data/FB15k-237/Hard \
+    -lr 0.00005 --max_steps 300001 --cpu_num 0 --valid_steps 30000 --test_batch_size 4 \
+    --seed 0 --drop 0.1 --tasks 2s.3s.sp.is.us.ins --checkpoint_path \
 
 ## Pretrain ConE * FB15k
-'''
 CUDA_VISIBLE_DEVICES=0 python3 main.py --cuda --do_train --do_test \
---tree_data_path /workspace/KGReasoning_Original/data/FB15k-betae -n 128 -b 512 -d 800 -g 40 --geo cone \
---dag_data_path /workspace/DAG-QA/data/FB15k/Hard \
+--tree_data_path ./data/FB15k-betae -n 128 -b 512 -d 800 -g 40 --geo cone \
+--dag_data_path ./DAG-QA/data/FB15k/Hard \
 -lr 0.00005 --max_steps 450001 --cpu_num 0 --valid_steps 60000 --test_batch_size 5 \
 --seed 0 --drop 0.05 --tasks 1p.2p.3p.2i.3i.ip.pi.2in.3in.inp.pin.pni.2u.up.2s.3s.sp.is.us.ins \
-'''
+
 ## test ConE * FB15k * Easy
 CUDA_VISIBLE_DEVICES=0 python3 main.py --do_test \
-    --tree_data_path /workspace/KGReasoning_Original/data/FB15k-betae \
-    --dag_data_path /workspace/DAG-QA/data/FB15k/Easy -n 128 -b 512 -d 800 -g 40 --drop 0.05 \
+    --tree_data_path ./data/FB15k-betae \
+    --dag_data_path ./DAG-QA/data/FB15k/Easy -n 128 -b 512 -d 800 -g 40 --drop 0.05 \
     --cpu_num 0 --geo cone --valid_steps 15000 -lr 0.00005 --max_steps 450001 \
-    --cuda --tasks 2s.3s.sp.is.us.ins --checkpoint_path  /workspace/DAG-QA/joint_rel/logs/FB15k/1p.2p.3p.2i.3i.ip.pi.2in.3in.inp.pin.pni.2u.up.2s.3s.sp.is.us.ins/cone/g-40.0-drop-0.05/2024.09.06-12:11:31\
+    --cuda --tasks 2s.3s.sp.is.us.ins --checkpoint_path  \
+    
+## test ConE * FB15k * Hard
+CUDA_VISIBLE_DEVICES=0 python3 main.py --do_test \
+    --tree_data_path ./data/FB15k-betae \
+    --dag_data_path ./DAG-QA/data/FB15k/Hard -n 128 -b 512 -d 800 -g 40 --drop 0.05 \
+    --cpu_num 0 --geo cone --valid_steps 15000 -lr 0.00005 --max_steps 450001 \
+    --cuda --tasks 2s.3s.sp.is.us.ins --checkpoint_path  \
